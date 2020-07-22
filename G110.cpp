dbae8fe4058420bd7ada58c18911c5626fedec66
Merlin Krümmel <merlin.kruemmel@gmx.net>
2020-07-23 01:01:41 +0200

    added license text and identifier in source files

--- G110.cpp
+++ G110.cpp
@@ -1,3 +1,32 @@
+/**
+  * copyright (c) 2020, Merlin Krümmel
+  * SPDX-License-Identifier: LGPL-3.0-or-later
+  */
+
+/**
+  * This library is free software: you can redistribute it and/or modify
+  * it under the terms of the GNU Lesser General Public License as published by
+  * the Free Software Foundation, version 3 or later.
+  *
+  * This program is distributed in the hope that it will be useful, but
+  * WITHOUT ANY WARRANTY; without even the implied warranty of
+  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+  * Lesser General Public License for more details.
+  *
+  * You should have received a copy of the GNU Lesser General Public License
+  * along with this program. If not, see <http://www.gnu.org/licenses/>.
+  */
+
+/**
+  *   @file   G110.cpp
+  *
+  *   @brief  class implementation for SINAMICS G100 drive inverter
+  *
+  *   @author Merlin Krümmel
+  *
+  *   @date   22.07.2020
+  */
+
 #include "G110.h"
 
 G110::G110()
--- USS.cpp
+++ USS.cpp
@@ -1,3 +1,32 @@
+/**
+  * copyright (c) 2020, Merlin Krümmel
+  * SPDX-License-Identifier: LGPL-3.0-or-later
+  */
+
+/**
+  * This library is free software: you can redistribute it and/or modify
+  * it under the terms of the GNU Lesser General Public License as published by
+  * the Free Software Foundation, version 3 or later.
+  *
+  * This program is distributed in the hope that it will be useful, but
+  * WITHOUT ANY WARRANTY; without even the implied warranty of
+  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+  * Lesser General Public License for more details.
+  *
+  * You should have received a copy of the GNU Lesser General Public License
+  * along with this program. If not, see <http://www.gnu.org/licenses/>.
+  */
+
+/**
+  *   @file   USS.cpp
+  *
+  *   @brief  class implementation for Siemens USS protocol
+  *
+  *   @author Merlin Krümmel
+  *
+  *   @date   22.07.2020
+  */
+
 #include "USS.h"
 
 extern USS uss;
